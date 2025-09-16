c768a3425500dedab25515b724408280df403d42
nurkhairunisanisa355-cmd <nurkhairunisanisa355@gmail.com>
2025-09-16 15:20:21 +0700

    first commit

---
+++ hitungluas.php
@@ -0,0 +1,14 @@
+<?php
+// Luas Persegi Panjang
+$panjang = 85;
+$lebar   = 80;
+$luas = $panjang * $lebar;
+$Keliling = 2 * ($panjang + $lebar);
+
+echo "<h1>Luas Persegi Panjang</h1>";
+echo "<p>Panjang = $panjang </p>";
+echo "<p>Lebar   = $lebar </p>";
+echo "<p>Luas    = $luas </p>";
+echo "<p>$panjang * $lebar : $luas</p>";
+echo "<p>Keliling = $Keliling</p>";
+?>
